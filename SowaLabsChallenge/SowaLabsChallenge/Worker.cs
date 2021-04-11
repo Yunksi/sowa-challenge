e944a46e06105e4f07af7f509155da79b4be7be4
Rok Erjavec <rok.erjavec@spica.com>
2021-04-11 22:36:58 +0200

    Adds Audit folder and notifications to connected clients

--- SowaLabsChallenge/SowaLabsChallenge/Worker.cs
+++ SowaLabsChallenge/SowaLabsChallenge/Worker.cs
@@ -1,6 +1,12 @@
+using System;
+using System.IO;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Hosting;
+using SowaLabsChallenge.Hubs;
 using SowaLabsChallenge.Services.Calculation;
 using SowaLabsChallenge.Services.FetchData;
 
@@ -10,31 +16,53 @@ namespace SowaLabsChallenge
     {
         private readonly IFetchDataService _fetchDataService;
         private readonly ICalculationService _calculationService;
+        private readonly IHubContext<OrderBookHub, IOrderBookHub> _orderBookHub;
+        private readonly IWebHostEnvironment _env;
 
         public Worker(
             IFetchDataService fetchDataService, 
-            ICalculationService calculationService)
+            ICalculationService calculationService,
+            IHubContext<OrderBookHub, IOrderBookHub> orderBookHub,
+            IWebHostEnvironment env)
         {
             _fetchDataService = fetchDataService;
             _calculationService = calculationService;
+            _orderBookHub = orderBookHub;
+            _env = env;
         }
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                //Fetch data from BINANCE API for BTC/EUR pair
+                // Fetch data from BINANCE API for BTC/EUR pair
                 var orderBookBtcEur =
                     await _fetchDataService.FetchOrderBookDataFromUrl("https://api.binance.com/api/v3/depth?symbol=BTCEUR&limit=1000");
-                //Fetch data from BINANCE API for BTC/USDC pair
+                // Fetch data from BINANCE API for BTC/USDC pair
                 var orderBookBtcUsd =
                     await _fetchDataService.FetchOrderBookDataFromUrl("https://api.binance.com/api/v3/depth?symbol=BTCUSDC&limit=1000");
-                //Calculate market depth
-                //Calculate order book depth for BTCEUR
+                // Calculate market depth
+                // Calculate order book depth for BTCEUR
                 var calculatedOrderBookDepthBtcEur = await _calculationService.CalculateOrderBookDepth(orderBookBtcEur);
                 // Calculate order book market depth for BTCUSDC
                 var calculatedOrderBookDepthBtcUsd = await _calculationService.CalculateOrderBookDepth(orderBookBtcUsd);
-                //Notify clients connected to SignalR hub
-                //Save returned data to file system
+                var serializedBtcEurOrderBookDepthDto = JsonSerializer.Serialize(calculatedOrderBookDepthBtcEur);
+                var serializedBtUsdOrderBookDepthDto = JsonSerializer.Serialize(calculatedOrderBookDepthBtcUsd);
+                // Notify clients connected to SignalR hub
+                // Notify all clients subscribed to BTCEUR
+                await _orderBookHub.Clients.Group("BTCEUR").UpdateOrderBook(serializedBtcEurOrderBookDepthDto);
+                // Notify all clients subscribed to BTCUSDC
+                await _orderBookHub.Clients.Group("BTCUSD").UpdateOrderBook(serializedBtUsdOrderBookDepthDto);
+                // Save returned data to file system
+                // For the sake of simplicity and purpose of the challenge we will write audit log to file system
+                // In the real case we would write audit to the database
+                using (var sw = File.CreateText($"{_env.ContentRootPath}/Audit/{DateTimeOffset.Now.ToUnixTimeSeconds()}_BTCEUR.txt"))
+                {
+                    await sw.WriteAsync(serializedBtcEurOrderBookDepthDto);
+                }
+                using (var sw = File.CreateText($"{_env.ContentRootPath}/Audit/{DateTimeOffset.Now.ToUnixTimeSeconds()}_BTCUSD.txt"))
+                {
+                    await sw.WriteAsync(serializedBtUsdOrderBookDepthDto);
+                }
                 await Task.Delay(10000);
             }
         }

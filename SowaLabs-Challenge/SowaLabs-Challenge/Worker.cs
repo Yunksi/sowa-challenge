fc416617c3908392d053e84d19b039c90582d113
Rok Erjavec <rok.erjavec@spica.com>
2021-04-11 13:26:20 +0200

    Adds background service to fetch data and notify clients

---
+++ SowaLabs-Challenge/SowaLabs-Challenge/Worker.cs
@@ -0,0 +1,21 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace SowaLabs_Challenge
+{
+    public class Worker: BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                //Fetch data from BINANCE API
+                //Calculate market depth
+                //Notify clients connected to SignalR hub
+                //Save returned data to file system
+                await Task.Delay(10000);
+            }
+        }
+    }
+}

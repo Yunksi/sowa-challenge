8574fe2633b0bd281f29a70c27b6197c9f53b948
Rok Erjavec <rok.erjavec@spica.com>
2021-04-11 14:49:30 +0200

    Adds unit tests for fetching data from bitstamp

--- SowaLabsChallenge/SowaLabsChallenge.Tests/FetchDataServiceTests.cs
+++ SowaLabsChallenge/SowaLabsChallenge.Tests/FetchDataServiceTests.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Threading.Tasks;
+using Shouldly;
 using SowaLabsChallenge.Services.FetchData;
 using Xunit;
 
@@ -13,9 +16,16 @@ namespace SowaLabsChallenge.Tests
         }
         
         [Fact]
-        public void Test1()
+        public async Task FetchDataFromUrl_ShouldThrowExceptionIfInvalidUrl()
         {
-
+            await Should.ThrowAsync<Exception>(_fetchDataService.FetchOrderBookDataFromUrl("test"));
+        }
+        
+        [Fact]
+        public async Task FetchDataFromUrl_ShouldReturnDeserializedObject()
+        {
+            var orderBook = await _fetchDataService.FetchOrderBookDataFromUrl("https://www.bitstamp.net/api/v2/order_book/btceur");
+            orderBook.ShouldNotBeNull();
         }
     }
 }

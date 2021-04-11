83c927338a428fcf7a0c39fbbd505f641d10f4b4
Rok Erjavec <rok.erjavec@spica.com>
2021-04-11 15:12:14 +0200

    Adds fetching data from Binance API to background service

--- SowaLabsChallenge/SowaLabsChallenge/Worker.cs
+++ SowaLabsChallenge/SowaLabsChallenge/Worker.cs
@@ -1,16 +1,28 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
+using SowaLabsChallenge.Services.FetchData;
 
 namespace SowaLabsChallenge
 {
     public class Worker: BackgroundService
     {
+        private readonly IFetchDataService _fetchDataService;
+
+        public Worker(IFetchDataService fetchDataService)
+        {
+            _fetchDataService = fetchDataService;
+        }
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                //Fetch data from BINANCE API
+                //Fetch data from BINANCE API for BTC/EUR pair
+                var orderBookBtcEur =
+                    await _fetchDataService.FetchOrderBookDataFromUrl("https://api.binance.com/api/v3/depth?symbol=BTCEUR&limit=1000");
+                //Fetch data from BINANCE API for BTC/USDC pair
+                var orderBookBtcUsd =
+                    await _fetchDataService.FetchOrderBookDataFromUrl("https://api.binance.com/api/v3/depth?symbol=BTCUSDC&limit=1000");
                 //Calculate market depth
                 //Notify clients connected to SignalR hub
                 //Save returned data to file system

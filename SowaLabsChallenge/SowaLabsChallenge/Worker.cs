bf2d2c8e7ffa6215fd9503fe0d3fdf6e9c48d14a
Rok Erjavec <rok.erjavec@spica.com>
2021-04-11 22:07:35 +0200

    Adds calculation to background service

--- SowaLabsChallenge/SowaLabsChallenge/Worker.cs
+++ SowaLabsChallenge/SowaLabsChallenge/Worker.cs
@@ -1,6 +1,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
+using SowaLabsChallenge.Services.Calculation;
 using SowaLabsChallenge.Services.FetchData;
 
 namespace SowaLabsChallenge
@@ -8,10 +9,14 @@ namespace SowaLabsChallenge
     public class Worker: BackgroundService
     {
         private readonly IFetchDataService _fetchDataService;
+        private readonly ICalculationService _calculationService;
 
-        public Worker(IFetchDataService fetchDataService)
+        public Worker(
+            IFetchDataService fetchDataService, 
+            ICalculationService calculationService)
         {
             _fetchDataService = fetchDataService;
+            _calculationService = calculationService;
         }
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
@@ -24,6 +29,10 @@ namespace SowaLabsChallenge
                 var orderBookBtcUsd =
                     await _fetchDataService.FetchOrderBookDataFromUrl("https://api.binance.com/api/v3/depth?symbol=BTCUSDC&limit=1000");
                 //Calculate market depth
+                //Calculate order book depth for BTCEUR
+                var calculatedOrderBookDepthBtcEur = await _calculationService.CalculateOrderBookDepth(orderBookBtcEur);
+                // Calculate order book market depth for BTCUSDC
+                var calculatedOrderBookDepthBtcUsd = await _calculationService.CalculateOrderBookDepth(orderBookBtcUsd);0
                 //Notify clients connected to SignalR hub
                 //Save returned data to file system
                 await Task.Delay(10000);
